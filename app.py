06616313b651137a1422031375c7e620d30f49bd
claudialyx <claudialyx@gmail.com>
2019-02-21 09:45:50 +0800

    added flask login to replace session login and logouts

--- app.py
+++ app.py
@@ -5,16 +5,18 @@ import config
 from flask import Flask, render_template, request, redirect, url_for, flash, session,escape
 from models.base_model import *
 from models.user import *
-# from flask_login import UserMixin, LoginManager, current_user,login_user, login_required
+from flask_login import LoginManager, current_user,login_user, login_required, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
+
+# how to use css styles using {%% } ??? 
 
 web_dir = os.path.join(os.path.dirname(
     os.path.abspath(__file__)), 'instagram_web')
 
 app = Flask('NEXTAGRAM', root_path=web_dir)
 csrf = CSRFProtect(app)
-# login_manager = LoginManager()
-# login_manager.init_app(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 if os.getenv('FLASK_ENV') == 'production':
     app.config.from_object("config.ProductionConfig")
@@ -30,9 +32,19 @@ def after_request(response):
     db.close()
     return response
 
-# @login_manager.user_loader
-# def load_user(user_id):
-#     return User.query.get(int(user_id))
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    # note that we set the 404 status explicitly
+    return render_template('500.html'), 500
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get_by_id(int(user_id))
 
 @app.route("/")
 def index():
@@ -42,7 +54,6 @@ def index():
 
 @app.route("/user")
 def index_user():
-    # login_user(user)
     return render_template('signin.html')
 
 @app.route("/user/sign_in", methods=['GET','POST'])
@@ -56,7 +67,9 @@ def sign_in():
             hashed_password = user.password
             result = check_password_hash(hashed_password, password_to_check)
             if result:
-                session["user_id"] = user.id # tells the browser to store `user.id` in session with the key as `user_id`
+                # session["user_id"] = user.id # tells the browser to store `user.id` in session with the key as `user_id`
+                login_user(user)
+                flash("The current user is" + current_user.username)
                 return redirect(url_for('index'))
             else: 
                 flash("Please ensure username and password are correct")
@@ -67,9 +80,11 @@ def sign_in():
     return render_template('signin.html')
 
 @app.route("/user/sign_out")
+@login_required
 def sign_out():
+    logout_user()
     # remove the username from the session if it's there
-    session.pop('user_id', None)
+    # session.pop('user_id', None)
     flash("Successfully signed out")
     return redirect(url_for('index'))
 
@@ -92,8 +107,8 @@ def create_user():
     new_user = User(username=user_username,email=user_email,password=hashed_password)
     
     if new_user.save():
-        flash("Successfully signed up! You may proceed to sign in now.")
-        return redirect(url_for('new_user'))
+        flash("Successfully signed up!")
+        return redirect(url_for('index'))
     else:
         breakpoint()
         return render_template('signup.html', errors=new_user.errors)
